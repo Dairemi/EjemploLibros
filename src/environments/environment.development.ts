7b5a00238f9d72f0c68502ff8e38a14abe01bdd5
Dayanna <yamilethpl.ti22@utsjr.edu.mx>
2025-03-10 13:47:07 -0600

    libros

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig : {
+      apiKey: "AIzaSyAVrQmiGK7y70z0wdkHzJ-BaJlpF6FZJxA",
+      authDomain: "libros-43d01.firebaseapp.com",
+      projectId: "libros-43d01",
+      storageBucket: "libros-43d01.firebasestorage.app",
+      messagingSenderId: "696723165446",
+      appId: "1:696723165446:web:c4b8d74803c9b739f14e87",
+      measurementId: "G-Q1V82MVQNF"
+      }
+};
